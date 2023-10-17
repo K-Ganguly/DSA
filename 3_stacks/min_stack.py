b1b5963b48620023e8af73df5fe9cfac63914195
Kushal Ganguly <kganguly.main@gmail.com>
2023-10-17 20:50:01 +0530

    fixed the formatting for min_stack.py

--- 3_stacks/min_stack.py
+++ 3_stacks/min_stack.py
@@ -1,41 +1,42 @@
 class MinStack:
     def __init__(self):
-        self.stack = list()  # Initialize the main stack to store values
-        self.min_value = (
-            None  # Initialize a variable to store the current minimum value
-        )
+        # Initialize the main stack to store values
+        self.stack = list()
+        # Initialize a variable to store the current minimum value
+        self.min_value = None
 
     def push(self, val: int) -> None:
+        # This is an initial marker for the first element.
         if not self.stack:
-            self.stack.append(0)  # This is an initial marker for the first element.
-            self.min_value = val  # Set the initial minimum as the first element.
+            self.stack.append(0)
+            # Set the initial minimum as the first element.
+            self.min_value = val
         else:
-            diff_with_min = (
-                val - self.min_value
-            )  # Calculate how the new value relates to the current minimum.
-            self.stack.append(
-                diff_with_min
-            )  # Store the difference instead of the value itself.
+            # Calculate how the new value relates to the current minimum.
+            diff_with_min = val - self.min_value
+            # Store the difference instead of the value itself.
+            self.stack.append(diff_with_min)
+            # Update the minimum when the new value is smaller.
             if diff_with_min < 0:
-                self.min_value = (
-                    val  # Update the minimum when the new value is smaller.
-                )
+                self.min_value = val
 
     def pop(self) -> None:
-        diff_with_min = self.stack.pop()  # Retrieve and process the stored difference.
+        # Retrieve and process the stored difference.
+        diff_with_min = self.stack.pop()
+        # Restore the previous minimum when necessary.
         if diff_with_min < 0:
-            self.min_value -= (
-                diff_with_min  # Restore the previous minimum when necessary.
-            )
+            self.min_value -= diff_with_min
 
     def top(self) -> int:
-        diff_with_min = self.stack[-1]  # Retrieve the stored difference at the top.
+        # Retrieve the stored difference at the top.
+        diff_with_min = self.stack[-1]
+        # Return the current minimum when applicable.
         if diff_with_min < 0:
-            return self.min_value  # Return the current minimum when applicable.
+            return self.min_value
         else:
-            return (
-                self.min_value + diff_with_min
-            )  # Adjust and return the actual value using the stored difference.
+            # Adjust and return the actual value using the stored difference.
+            return self.min_value + diff_with_min
 
     def getMin(self) -> int:
-        return self.min_value  # Simply return the current minimum value.
+        # Simply return the current minimum value.
+        return self.min_value
